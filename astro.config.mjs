65f49791a1af41a5ebf7aeaf7f5bf5b2c411b0e4
Bruno MAUCOURT <bruno@sigmapix.com>
2026-09-10 14:17:16 +0200

    Ajout TailwindCSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bruno-sigmapix.github.io',
   base: '/tina/',
   integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
