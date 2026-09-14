6c1e804023bf21832c709ed613762fed50b33c8e
Bruno MAUCOURT <bruno@sigmapix.com>
2026-09-14 12:38:53 +0200

    Configuration pour tina-cms.sigmapix.fr - 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tina-cms.sigmapix.fr',
-  base: '/tina/',
   integrations: [react()],
 
   vite: {
